f8596a93b634168e9466aa55bf3ce37eb8d952e3
Tristan Davies <github@tristan.io>
2015-01-02 16:51:19 -0500

    major refactor, version 1.0.0-rc.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,26 @@
+## 1.0.0-rc.0 (2015-01-02)
+
+Major refactor using through2 streams for cleaner and safer code
+
+Features:
+
+  - `--output-split [column]` option. Writes to multiple output files based on a column name. Can be a column that is fetched from smartystreets, such as state. If specified, `--output` will be a directory instead of a file.
+  - `--log-interval [rows]` option. Log every X number of rows. Was previously hard-coded to 1000. Default is 1000.
+  - `--quiet` option. Actually works now. Suppresses non-fatal errors and progress messages.
+
+Bugs:
+
+  - Elusive memory bugs sorted out by switching to through2 and thus preventing backpressure issues caused by poorly implemented streams
+
+Features removed:
+
+  - Redis caching. Who needs it? Not me. It was a bad idea anyways.
+
 ## 0.8.0 (2014-12-30)
 
 Features:
 
- - --delimiter option
+ - `--delimiter` option
 
 ## 0.7.0 (2014-09-12)
 
--- program.js
+++ program.js
@@ -1,12 +1,15 @@
-#!/usr/bin/env node
-
-var Smartystreets = require('./');
-var program = require('commander');
+var csv = require('fast-csv');
 var fs = require('fs');
+var options = require('commander');
+var grouper = require('./lib/grouper');
+var geocoder = require('./lib/geocoder');
+var streamSplitter = require('./lib/streamSplitter');
+var through2 = require('through2');
+var terminus = require('terminus');
 
 var package = JSON.parse(fs.readFileSync(__dirname+'/package.json', 'utf8'));
 
-program
+options
   .version(package.version)
   .option('-i, --input [file]', 'Input (csv file) [stdin]', '-')
   .option('-o, --output [file]', 'Output (csv file) [stdout]', '-')
@@ -14,88 +17,95 @@ program
   .option('-z, --zipcode-col [col]', 'Zipcode col [zipcode]', 'zipcode')
   .option('-c, --city-col [col]', 'City col [city]', 'city')
   .option('-S, --state-col [col]', 'State col [state]', 'state') // short forms are dumb
-  .option('-d, --delimiter [symbol]', 'CSV delimiter in input file', ',') // short forms are dumb
+  .option('-d, --delimiter [symbol]', 'CSV delimiter in input file', ',')
+  .option('-O, --output-split [column]', 'Write to multiple files divided by column', '')
   .option('-a, --auth-id [id]', 'SmartyStreets auth id [environment variable smartystreets_auth_id]', process.env.SMARTYSTREETS_AUTH_ID)
   .option('-A, --auth-token [token]', 'SmartyStreets auth token [environment variable smartystreets_auth_token]', process.env.SMARTYSTREETS_AUTH_TOKEN)
   .option('-j, --concurrency [jobs]', 'Maximum number of concurrent requests [48]', 48)
-  .option('-✈, --column-definition [mode|file|string]', 'Column definition mode or file [standard]', 'standard')
+  .option('-✈, --column-definition [mode]', 'Column definition mode or file [standard]', 'standard')
   .option('-p, --column-prefix [text]', 'Prefix for smartystreets columns in the output file [ss_]', 'ss_')
   .option('-x, --column-suffix [text]', 'Suffix for smartystreets columns in the output file', '')
-  .option('-r, --redis [url]', 'Redis cache url')
-  .option('-q, --quiet', "Quiet mode - turn off progress messages")
+  .option('-q, --quiet', 'Quiet mode - turn off progress messages')
+  .option('-l, --log-interval [num]', 'Show progress after every X number of rows [1000]', 1000)
   .parse(process.argv);
 
-program.concurrency = parseInt(program.concurrency);
+options.concurrency = parseInt(options.concurrency);
 
-if (!program.concurrency) {
+if (!options.concurrency) {
   console.error('Invalid concurrency');
-  program.help();
+  options.help();
 }
 
-if (!program.authId) {
+if (!options.authId) {
   console.error('Please specify a SmartyStreets auth id');
-  program.help();
+  options.help();
 }
 
-if (!program.authToken) {
+if (!options.authToken) {
   console.error('Please specify a SmartyStreets auth token');
-  program.help();
+  options.help();
 }
 
-if (!program.input) {
+if (!options.input) {
   console.error('Please specify an input file');
-  program.help();
+  options.help();
 }
-if (!program.output) {
+if (!options.output) {
   console.error('Please specify an input file');
-  program.help();
+  options.help();
 }
 
 var columnModes = ['mail', 'standard', 'complete'];
-if (columnModes.indexOf(program.columnDefinition) != -1) {
+if (columnModes.indexOf(options.columnDefinition) != -1) {
   //modes are in structure/mode.json
-  program.structure = JSON.parse( fs.readFileSync(__dirname+'/structure/'+program.columnDefinition+'.json', 'utf8') );
-} else if (program.columnDefinition.substr(0, 1) == '{') {
+  options.structure = JSON.parse( fs.readFileSync(__dirname+'/structure/'+options.columnDefinition+'.json', 'utf8') );
+} else if (options.columnDefinition.substr(0, 1) == '{') {
   //it's a JSON object
-  program.structure = JSON.parse(program.columnDefinition);
+  options.structure = JSON.parse(options.columnDefinition);
 } else {
   //it's a JSON file
-  program.structure = JSON.parse( fs.readFileSync(program.columnDefinition, 'utf8') );
+  options.structure = JSON.parse( fs.readFileSync(options.columnDefinition, 'utf8') );
 }
 
-var inputStream;
-if (program.input == '-') {
-  console.error('Reading from stdin');
-  inputStream = process.stdin;
-  process.stdin.resume();
-} else {
-  inputStream = fs.createReadStream(program.input);
-}
 
-var outputStream;
-if (program.output == '-') {
-  console.error('Writing to stdout');
-  outputStream = process.stdout;
-} else {
-  outputStream = fs.createWriteStream(program.output);
-}
+var readStream = options.input == '-' ? process.stdin : fs.createReadStream(options.input);
 
-var geocodingStream = new Smartystreets(program);
+var writeStream;
+if (options.outputSplit) {
+  fs.mkdirSync(options.output);
 
-//this is where the magic happens
-inputStream.pipe(geocodingStream).pipe(outputStream);
+  var firstRow = true, rowIndex, headers;
 
-if (!program.quiet) {
-  var progressInterval = 1000;
-  var nextProgressMessage = progressInterval;
+  writeStream = streamSplitter(function(row){
+    if (firstRow) {
+      firstRow = false;
+      rowIndex = row.indexOf(options.outputSplit);
+      headers = row;
 
-  geocodingStream.on('progress', function(progress, done){
-    if (done || progress.total >= nextProgressMessage) {
+      if (rowIndex == -1) {
+        console.error('couldn\'t find column', options.outputSplit, 'in', row);
+        process.exit();
+      }
 
-      var percentage = Math.round((progress.geocoded / progress.total) * 100);
-      console.error('['+program.input+' -> '+program.output+'] '+progress.total + ' rows processed, ' + progress.geocoded + ' rows geocoded ('+percentage+'%), ' + progress.cached + ' rows cached');
-
-      nextProgressMessage += progressInterval;
+      return false;
     }
-  });
+
+    return row[rowIndex];
+  }, function(streamName){
+    var source = csv.createWriteStream();
+    source.write(headers);
+    source.pipe( fs.createWriteStream(options.output+'/'+streamName+'.csv') );
+    return source;
+  }, {objectMode: true});
+} else if (options.output == '-') {
+  writeStream = csv.createWriteStream();
+  writeStream.pipe( process.stdout );
+} else {
+  writeStream = csv.createWriteStream();
+  writeStream.pipe( fs.createWriteStream(options.output+'/'+streamName+'.csv') );
 }
+
+readStream.pipe(csv({headers: true, delimiter: options.delimiter}))
+  .pipe(grouper(70))
+  .pipe(geocoder(options))
+  .pipe(writeStream);
